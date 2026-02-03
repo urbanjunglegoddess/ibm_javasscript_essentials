314b8876cb1a79a9aeb8637fda28ac507f6a2845
Omegea Hunter <omegeadhunter@gmail.com>
2026-02-03 05:45:45 -0500

    Added functions to controlFlow.js

--- controlFlow/controlFlow.js
+++ controlFlow/controlFlow.js
@@ -10,3 +10,37 @@ if (userRole === "admin") {
 }
 
 console.log("Access Level:", accessLevel);
+
+let isLoggedIn = true;
+let userMessage;
+
+if (isLoggedIn) {
+  if (userRole === "admin") {
+    userMessage = "Welcome, Admin!";
+  } else {
+    userMessage = "Welcome, User!";
+  }
+} else {
+  userMessage = "Please log in to access the system.";
+}
+
+console.log("User Message:", userMessage);
+
+let userType = "subscriber";
+let userCategory;
+
+switch (userType) {
+  case "admin":
+    userCategory = "Administrator";
+    break;
+  case "manager":
+    userCategory = "Manager";
+    break;
+  case "subscriber":
+    userCategory = "Subscriber";
+    break;
+  default:
+    userCategory = "Unknown";
+}
+
+console.log("User Category:", userCategory);
